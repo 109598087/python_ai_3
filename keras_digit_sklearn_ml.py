5c18fcbfd5e3574912b6324a0bbf661d9adfb45b
109598087 <t109598087@ntut.org.tw>
2021-01-27 16:09:58 +0800

    ke

--- keras_digit_sklearn_ml.py
+++ keras_digit_sklearn_ml.py
@@ -1,26 +1,14 @@
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
+from keras.datasets import mnist
 from sklearn.tree import DecisionTreeClassifier
-import matplotlib.pyplot as plt
-import numpy as np
 
-digits = datasets.load_digits()
-X = digits['data']
-y = digits['target']
+X_train = mnist.load_data()[0][0]
+y_train = mnist.load_data()[0][1]
 
-# # draw
-# plt.imshow(X[0].reshape(8, 8), 'gray_r')
-# plt.show()
+X_test = mnist.load_data()[1][0]
+y_test = mnist.load_data()[1][1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-# print(X, y)
-# print(X_train.shape, X_test.shape)
+print(X_train.shape)
+
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
-print(model.score(X_test, y_test))
-
-# 取機率最大的
-# print(model.predict_proba(X_test))
-# print(np.argmax(model.predict_proba(X_test), axis=1)[:10])  # axis=1: 從row中選出max的位置(機率最大的)
-
-print(np.average(model.predict(X_test) == y_test))
+# model.score()
