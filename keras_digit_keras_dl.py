149ce06690c77837f9bc3ff836a503c8e7d3db13
109598087 <t109598087@ntut.org.tw>
2021-01-27 16:55:20 +0800

    keras-digit-keras-dl ok

---
+++ keras_digit_keras_dl.py
@@ -0,0 +1,34 @@
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+
+# get data
+from keras.utils import to_categorical
+
+X_train = mnist.load_data()[0][0]
+y_train = mnist.load_data()[0][1]
+print(X_train.shape)
+print(y_train.shape)
+X_test = mnist.load_data()[1][0]
+y_test = mnist.load_data()[1][1]
+print(X_test.shape)
+print(y_test.shape)
+
+# build model
+model = Sequential()
+model.add(Dense(128, activation='relu', input_shape=(28 * 28,)))  # input_data需要flatten
+model.add(Dense(10, activation='softmax'))  # 128(神經元)輸出 --> 多(0-9)選一
+model.summary()
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train.reshape(60000, 28 * 28), to_categorical(y_train),
+          batch_size=1000,
+          epochs=10,
+          verbose=2,  # 顯示過程
+          validation_split=0.2)
+# 很多True、False
+print(np.argmax(model.predict(X_test.reshape(10000, 28 * 28)), axis=1) == y_test)
+# 把很多True、False average->精準度
+print(np.average(np.argmax(model.predict(X_test.reshape(10000, 28 * 28)), axis=1) == y_test))
--- sklearn_digit_keras_dl.py
+++ sklearn_digit_keras_dl.py
@@ -1,19 +1,8 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
 
 # get data
 digits = datasets.load_digits()
 X = digits['data']
 y = digits['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-
-print(X_train.shape)
-print(X_test.shape)
-print(y_train.shape)
-print(y_test.shape)
-
-model = DecisionTreeClassifier()
-model.fit(X_train, y_train)
-print(model.score(X_test, y_test))  # 0.8367003367003367
