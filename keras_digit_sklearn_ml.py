5df5645fa1594f3699b61633dbc0b92d40458722
109598087 <t109598087@ntut.org.tw>
2021-01-27 16:30:06 +0800

    sklearn_digit、keras_digit sklearn_ml ok

--- keras_digit_sklearn_ml.py
+++ keras_digit_sklearn_ml.py
@@ -1,14 +1,15 @@
 from keras.datasets import mnist
 from sklearn.tree import DecisionTreeClassifier
 
-X_train = mnist.load_data()[0][0]
+X_train = mnist.load_data()[0][0].reshape(60000, 28 * 28)
 y_train = mnist.load_data()[0][1]
-
-X_test = mnist.load_data()[1][0]
-y_test = mnist.load_data()[1][1]
-
 print(X_train.shape)
+print(y_train.shape)
+X_test = mnist.load_data()[1][0].reshape(10000, 28 * 28)
+y_test = mnist.load_data()[1][1]
+print(X_test.shape)
+print(y_test.shape)
 
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
-# model.score()
+print(model.score(X_test, y_test))
